5b693c9a110e4b33b64b272ef5c42b364c3e00b2
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-02-08 11:23:38 -0600

    PTL the text field and error messages are working, over the hump!

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 class ViewController: UIViewController {
     
    
+    @IBOutlet weak var usdTextField: UITextField!
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    @IBOutlet weak var eurSwitch: UISwitch!
+    @IBOutlet weak var gbpSwitch: UISwitch!
+    @IBOutlet weak var jnySwitch: UISwitch!
+    
+    
+    
+    @IBAction func convertButtonTapped(_ sender: UIButton) {
+        
+        // Read the text safely
+        guard let usdText = usdTextField.text, !usdText.isEmpty else {
+            errorMessage.text = "Please enter an amount."
+            return
+        }
+        
+        // Convert to int
+        guard let usdAmount = Int(usdText) else {
+            errorMessage.text = "Invalid input. Enter integers only."
+            return
+        }
+        
+        // Clear the error message
+        errorMessage.text = ""
+    }
     
     
     
