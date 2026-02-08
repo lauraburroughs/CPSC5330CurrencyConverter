7e231a0da919e830dc34cbf227373f86a035e642
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-02-08 13:29:41 -0600

    Segue mostly working, needs polishing up

--- CurrencyConverter/NavigationViewController.swift
+++ CurrencyConverter/NavigationViewController.swift
@@ -15,18 +15,55 @@ class NavigationViewController: UIViewController {
     var gbpSelected: Bool = false
     var jpySelected: Bool = false
     
+    
+    
+    @IBOutlet weak var eurLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdAmountLabel: UILabel!
+    
+    
+    
+    func convertCurrency(amount: Int, rate: Double) -> Double {
+        return Double(amount) * rate
+    }
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // USD entered
+        usdAmountLabel.text = "\(usdAmount)"
+        
+        
+        // EUR
+        if eurSelected {
+            let eurValue = convertCurrency(amount: usdAmount, rate: 0.92)
+            eurLabel.text = "\(usdAmount) USD = \(Int(eurValue)) EUR"
+            eurLabel.isHidden = false
+        } else {
+            eurLabel.isHidden = true
+        }
+        
+        // GBP
+        if gbpSelected {
+            let gbpValue = convertCurrency(amount: usdAmount, rate: 0.79)
+            gbpLabel.text = "\(usdAmount) USD = \(Int(gbpValue)) GBP"
+            gbpLabel.isHidden = false
+        } else {
+            gbpLabel.isHidden = true
+        }
+        
+        // JPY
+        if jpySelected {
+            let jpyValue = convertCurrency(amount: usdAmount, rate: 140.0)
+            jpyLabel.text = "\(usdAmount) USD = \(Int(jpyValue)) JPY"
+            jpyLabel.isHidden = false
+        } else {
+            jpyLabel.isHidden = true
+        }
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    */
 
 }
--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,68 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Variables hold user input
     var usdAmount: Int = 0
     var eurSelected: Bool = false
     var gbpSelected: Bool = false
     var jpySelected: Bool = false
     
-   
+    
+    // IB Outlets
     @IBOutlet weak var usdTextField: UITextField!
     @IBOutlet weak var errorMessage: UILabel!
-    
     @IBOutlet weak var eurSwitch: UISwitch!
     @IBOutlet weak var gbpSwitch: UISwitch!
     @IBOutlet weak var jpySwitch: UISwitch!
     
     
-    
+    // Button Action
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        
-        // Read the text safely
-        guard let usdText = usdTextField.text, !usdText.isEmpty else {
-            errorMessage.text = "Please enter an amount."
-            return
-        }
-        
-        // Convert to int
-        guard let amount = Int(usdText) else {
-            errorMessage.text = "Invalid input. Enter integers only."
-            return
-        }
-        self.usdAmount = amount
-        
-        
-        // Clear the error message
-        errorMessage.text = ""
-        
-        // Read which currencies to do
-        eurSelected = eurSwitch.isOn
-        gbpSelected = gbpSwitch.isOn
-        jpySelected = jpySwitch.isOn
-        
-        // Pass to Screen 2
-    print("USD: \(usdAmount), EUR: \(eurSelected), GBP: \(gbpSelected), JPY: \(jpySelected)")
+        print("Convert buttong tapped")
     }
     
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "showResults" {
+            
+            // Validate USD input
+            guard let usdText = usdTextField.text, !usdText.isEmpty else {
+                errorMessage.text = "Please enter an amount."
+                return false
+            }
+            
+            guard let amount = Int(usdText) else {
+                errorMessage.text = "Invalid input. Enter integers only."
+                return false
+            }
+            
+            
+            // Save values for prepare(for:) to use
+            usdAmount = amount
+            eurSelected = eurSwitch.isOn
+            gbpSelected = gbpSwitch.isOn
+            jpySelected = jpySwitch.isOn
+            
+            
+            // Clear error message
+            errorMessage.text = ""
+            return true
+        }
+        
+        return true
+    }
+        
+    
+    
+    // Pass data to Screen 2
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showResults" {
             let destination = segue.destination as! NavigationViewController
-            
             destination.usdAmount = usdAmount
             destination.eurSelected = eurSelected
             destination.gbpSelected = gbpSelected
             destination.jpySelected = jpySelected
-            
         }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
     }
-
 }
 
