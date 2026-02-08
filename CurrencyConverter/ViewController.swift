1a193afccc8d6f8e28a2eb98634a3bd9cac691bf
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-02-08 09:51:16 -0600

    Added outlets and button action

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var usdTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
 
+    @IBOutlet weak var usdToEurSwitch: UISwitch!
+    @IBOutlet weak var usdToJpySwitch: UISwitch!
+    @IBOutlet weak var usdToGbpSwitch: UISwitch!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
+    @IBAction func convertButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
 
 }
 
